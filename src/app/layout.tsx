7cee414c0485ebfab5f05d021a2bfee47883256d
ryanwolhuter <dev@ryanwolhuter.com>
2023-07-31 14:50:53 +0200

    add bg
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
-import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import "./globals.css";
 import { Providers } from "./providers";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className="bg-gray-50 bg-no-repeat bg-top"
+        style={{
+          backgroundImage: "url('/background.svg')",
+        }}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
