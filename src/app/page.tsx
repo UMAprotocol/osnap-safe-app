cf3823a7597075fc9860ba8a79f62e2d45c36bbe
ryanwolhuter <dev@ryanwolhuter.com>
2023-08-01 16:16:10 +0200

    add responsive logo to look nice in the mean time
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@/components/Icon";
 export default function Page() {
   return (
     <main className="w-screen h-screen grid place-items-center">
-      <Icon name="osnap-logo" />
+      <Icon name="osnap-logo" className="w-[50vw] aspect-[10/3]" />
     </main>
   );
 }
