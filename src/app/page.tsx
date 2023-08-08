b267c8dcc878ee71f9ab7bd9993a71a980fc5bf8
ryanwolhuter <dev@ryanwolhuter.com>
2023-08-08 13:45:04 +0200

    lint
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Icon } from "@/components/Icon";
 
 export default function Page() {
   return (
-    <main className="w-screen h-screen grid place-items-center">
-      <Icon name="osnap-logo" className="w-[50vw] aspect-[10/3]" />
+    <main className="grid h-screen w-screen place-items-center">
+      <Icon name="osnap-logo" className="aspect-[10/3] w-[50vw]" />
     </main>
   );
 }
