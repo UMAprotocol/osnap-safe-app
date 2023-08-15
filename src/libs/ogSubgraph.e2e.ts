485ca4651ff4d5178d7860fa15e8d869fcf33356
david <david@umaproject.org>
2023-08-15 09:10:02 -0400

    feat: add basic og subgraph calls
    Signed-off-by: david <david@umaproject.org>

---
+++ src/libs/ogSubgraph.e2e.ts
@@ -0,0 +1,22 @@
+// this wont run unless tsconfig is set from "module": "esnext" to module: commonjs
+import test from "tape";
+
+import { Client } from "./ogSubgraph";
+
+const testSafe = "0x504323601078746bCDf2AC517BF80f7736853c2e";
+const moduleAddress = "0x9655Dd68bA1b3D2b6d49Ea67b6b405C2cA1615fD";
+
+test("ogSubgraph", (t) => {
+  t.test("getModuleAddress", async (t) => {
+    const client = Client(5);
+    const address = await client.getModuleAddress(testSafe);
+    t.equals(address, moduleAddress);
+    t.end();
+  });
+  t.test("isOsnapEnabled", async (t) => {
+    const client = Client(5);
+    const enabled = await client.isOSnapEnabled(testSafe);
+    t.ok(enabled);
+    t.end();
+  });
+});
---
+++ src/libs/ogSubgraph.ts
@@ -0,0 +1,46 @@
+import assert from "assert";
+import request, { gql } from "graphql-request";
+import { findContract } from "./contracts";
+import { ethers } from "ethers";
+
+export function Client(chainId: number) {
+  const ogInfo = findContract({ chainId, name: "OptimisticGovernor" });
+  assert(ogInfo.subgraph, `No subgraph defined for OG on chainId ${chainId}`);
+  const subgraph = ogInfo.subgraph;
+
+  async function getModuleAddress(safeAddress: string): Promise<string> {
+    type Response = {
+      safe: { optimisticGovernor: { id: string } };
+    };
+    const gqlQuery = gql`
+      query getModuleAddress {
+        safe(id:"${safeAddress.toLowerCase()}"){
+          optimisticGovernor {
+            id
+          }
+        }
+      }
+    `;
+    const response = await request<Response>(subgraph, gqlQuery);
+    return ethers.utils.getAddress(response.safe.optimisticGovernor.id);
+  }
+  async function isOSnapEnabled(safeAddress: string): Promise<boolean> {
+    type Response = {
+      safe: { isOptimisticGovernorEnabled: boolean };
+    };
+    const gqlQuery = gql`
+      query isOSnapEnabled {
+        safe(id:"${safeAddress.toLowerCase()}"){
+          isOptimisticGovernorEnabled
+        }
+      }
+    `;
+    const response = await request<Response>(subgraph, gqlQuery);
+    return response.safe.isOptimisticGovernorEnabled;
+  }
+
+  return {
+    isOSnapEnabled,
+    getModuleAddress,
+  };
+}
