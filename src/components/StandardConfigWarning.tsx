90735d38c8aacce8dbd5f33e148954e822d1ad03
Gerhard Steenkamp <gerhard@umaproject.org>
2024-02-16 15:57:09 +0200

    use green for standard settings

--- src/components/StandardConfigWarning.tsx
+++ src/components/StandardConfigWarning.tsx
@@ -3,22 +3,26 @@ type Props = {
 };
 
 export const StandardConfigWarning = ({ isStandard }: Props) => {
-  return (
-    <div className="mb-6 rounded-lg  border bg-warning-50 px-3 py-2 text-sm text-warning-700">
-      {isStandard ? (
+  if (isStandard) {
+    return (
+      <div className="mb-6 rounded-lg  border bg-success-50 px-3 py-2 text-sm text-success-700">
         <p>
           Warning! You are using the default settings. If your proposal passes,
           your transaction will be <strong>automatically executed</strong> and
           verified by the UMA Optimistic Oracle.
         </p>
-      ) : (
-        <p>
-          Warning! You are <strong>not</strong> using the default settings. If
-          your proposal passes, you will be required to{" "}
-          <strong>manually</strong> request transaction execution and post a
-          bond to the UMA Optimistic Oracle for verification.
-        </p>
-      )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="mb-6 rounded-lg  border bg-warning-50 px-3 py-2 text-sm text-warning-700">
+      <p>
+        Warning! You are <strong>not</strong> using the default settings. If
+        your proposal passes, you will be required to <strong>manually</strong>{" "}
+        request transaction execution and post a bond to the UMA Optimistic
+        Oracle for verification.
+      </p>
     </div>
   );
 };
